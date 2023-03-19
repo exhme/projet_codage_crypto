1c575a4f9506302a285034fbffe9235b6ee94846
charlesboutardV33 <83752524+charlesboutardV33@users.noreply.github.com>
2023-03-19 22:53:55 +0100

    ajout de l'interface

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,13 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
+
+        View vue = new View();
         System.out.println("bonjour");
-        Helper helper = new Helper();
-        System.out.println(helper.getCONVERT_MAP());
-        ArrayList<Integer> messageEnInt = helper.strToInt("bonjour");
-        HashMap<Integer,Carte> cards = helper.buildAllCard();
+
+
+
+
 
         
     }
