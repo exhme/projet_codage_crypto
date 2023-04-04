43f773813f8916ac88b8ea89b39321e26f57ae70
Charles Boutard <boutardcharles@gmail.com>
2023-04-04 17:12:58 +0200

    passage d'une hashmap a une arrayliste pour gerer la conversion caracvtere <-> int

--- src/Caractere.java
+++ src/Caractere.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class caractere {
+public class Caractere {
+    private char lettre;
+    private int value;
+
+    public Caractere(char lettre, int value) {
+        this.lettre = lettre;
+        this.value = value;
+    }
+
+    public char getLettre() {
+        return lettre;
+    }
+
+    public void setLettre(char lettre) {
+        this.lettre = lettre;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
